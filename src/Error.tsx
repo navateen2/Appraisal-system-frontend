9380d891506173bf2c5a3ee1690e574f15b53a58
navaneet <navateen2@gmail.com>
2026-06-30 15:19:12 +0530

    add: added main, error and page-router

---
+++ src/Error.tsx
@@ -0,0 +1,11 @@
+import "./error.css"
+
+function Error(){
+    return(<div className="error-page">
+   <span className="nf">404 Not Found</span>
+   <span className="pnf">This page doesn't exist.</span>
+    
+    </div>)
+}
+
+export default Error;
